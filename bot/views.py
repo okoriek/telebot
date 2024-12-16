841a573f728ea1810f5ec11cf74cad1ecd8644a1
okoriek <okoriek55@gmail.com>
2024-12-16 16:33:53 +0100

    updated bot

--- bot/views.py
+++ bot/views.py
@@ -11,7 +11,7 @@ from django.http import JsonResponse
 from django.conf import settings
 
 def set_telegram_webhook(request):
-    TELEGRAM_TOKEN = settings.TELEGRAM_TOKEN  # Set your token in settings.py
+    TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN') # Set your token in settings.py
     TELEGRAM_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/setWebhook"
 
     # Your webhook URL on Render
