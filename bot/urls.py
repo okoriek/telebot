7f09d1a3724e9d624bd8d702db4ad3be5784ed1c
okoriek <okoriek55@gmail.com>
2024-12-16 16:26:34 +0100

    update view

--- bot/urls.py
+++ bot/urls.py
@@ -2,7 +2,9 @@
 # urls.py
 from django.urls import path
 from .views import telegram_webhook
+from . import views
 
 urlpatterns = [
     path("telegram-webhook/", telegram_webhook, name="telegram_webhook"),
+    path('', views.set_telegram_webhook, name='webhook'),
 ]
--- bot/views.py
+++ bot/views.py
@@ -5,6 +5,28 @@ import json
 
 import os
 
+
+import requests
+from django.http import JsonResponse
+from django.conf import settings
+
+def set_telegram_webhook(request):
+    TELEGRAM_TOKEN = settings.TELEGRAM_TOKEN  # Set your token in settings.py
+    TELEGRAM_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/setWebhook"
+
+    # Your webhook URL on Render
+    webhook_url = "https://telebot-p95r.onrender.com/bot/telegram-webhook/"
+
+    # Send the POST request to set the webhook
+    response = requests.post(TELEGRAM_URL, data={"url": webhook_url})
+
+    # Return the response from Telegram API
+    if response.status_code == 200:
+        return JsonResponse({"status": "success", "message": "Webhook set successfully."})
+    else:
+        return JsonResponse({"status": "error", "message": "Failed to set webhook."})
+
+
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 TELEGRAM_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}"
 
